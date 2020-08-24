5aca61ce193ca72f3dd6ad1c71aa4b813a43b0c1
Blaza8312 <61658959+Blaza8312@users.noreply.github.com>
2020-08-24 16:46:31 +0200

    Add files via upload

--- components/Burger/Burger.js
+++ components/Burger/Burger.js
@@ -8,15 +8,33 @@ import BurgerIngridient from './BurgerIngridient/BurgerIngridient'
 
 const burger = (props) => {
     
-console.log('solve it')
+props = {
+    ingridients: {
+        salad: 0,
+        bacon: 0,
+        cheese:0,
+        meat:0
+    }
+}
 
+let transformedIngridients = Object.keys(props.ingridients).map(igKey => {
+    return [...Array(props.ingridients[igKey])].map((_, i) => {
+        return <BurgerIngridient key={igKey +i} type = {igKey} />
+    })
+}).reduce((arr, el) => {
+return arr.concat(el)
+}, [])
+if (transformedIngridients.length === 0){
+     transformedIngridients = <p>Please start adding ingredients!</p>
+}
+ 
 
     return (
         <div className={classes.Burger}>
             <BurgerIngridient type='bread-top'/>
+            {transformedIngridients}
             <BurgerIngridient type='bread-bottom'/>
-            <BurgerIngridient type='meat'/>
-            <BurgerIngridient type='cheese'/>
+            
             
            
         </div>
